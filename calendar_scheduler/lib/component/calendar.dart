b06fdad8c2241022747e65eb592557460267442a
kimjunyeob <tislwlstnf1@naver.com>
2023-04-09 16:46:27 +0900

    2023-04-09 datetime statefull로 작업

--- calendar_scheduler/lib/component/calendar.dart
+++ calendar_scheduler/lib/component/calendar.dart
@@ -1,19 +1,27 @@
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({Key? key}) : super(key: key);
+class Calendar extends StatelessWidget {
+  final DateTime selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
-  DateTime focusedDay = DateTime.now();
+  const Calendar(
+      {required this.selectedDay,
+      required this.focusedDay,
+      required this.onDaySelected,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final defaultBoxDeco = BoxDecoration(
+        color: Colors.grey[200], borderRadius: BorderRadius.circular(6));
+
+    final defaultTextStyle =
+        TextStyle(color: Colors.grey[600], fontWeight: FontWeight.w700);
+
     return TableCalendar(
       locale: 'ko_KR',
       focusedDay: focusedDay,
@@ -23,18 +31,29 @@ class _CalendarState extends State<Calendar> {
           formatButtonVisible: false,
           titleCentered: true,
           titleTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 16)),
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        setState(() {
-          this.selectedDay = selectedDay;
-          this.focusedDay = focusedDay;
-        });
-      },
+      onDaySelected: onDaySelected,
       selectedDayPredicate: (DateTime day) {
         // 시분초까지 같을 필요는 없기 때문에 아래 분기로 코딩
         return day.year == selectedDay?.year &&
             day.month == selectedDay?.month &&
             day.day == selectedDay?.day;
       },
+      calendarStyle: CalendarStyle(
+          todayDecoration: defaultBoxDeco.copyWith(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6),
+            border: Border.all(color: PRIMARY_COLOR, width: 1),
+          ),
+          todayTextStyle: defaultTextStyle,
+          defaultTextStyle: defaultTextStyle,
+          defaultDecoration: defaultBoxDeco,
+          weekendDecoration: defaultBoxDeco,
+          weekendTextStyle: defaultTextStyle,
+          selectedDecoration: defaultBoxDeco.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+          selectedTextStyle: defaultTextStyle.copyWith(color: Colors.white),
+          outsideDecoration: BoxDecoration(shape: BoxShape.rectangle)),
     );
   }
 }
--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'screen/home_screen.dart';
 
-void main() {
-  initializeDateFormatting().then((_) => runApp(MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: "NotoSans"),
-        home: HomeScreen(),
-      )));
+void main() async {
+  // 방법 1
+  // await initializeDateFormatting().then((_) => runApp(MaterialApp(
+  //       debugShowCheckedModeBanner: false,
+  //       theme: ThemeData(fontFamily: "NotoSans"),
+  //       home: HomeScreen(),
+  //     )));
+
+  // 방법2
+  WidgetsFlutterBinding.ensureInitialized(); // 1. flutter를 실행한 준비를 확인
+  await initializeDateFormatting(); // 2. intl formatting 설정
+  runApp(MaterialApp( // 3. runApp 실행
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: "NotoSanas",
+    ),
+    home: HomeScreen(),
+  ));
 }
