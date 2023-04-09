22750e6b30ffa47b437338d7949be5bcb8b4da46
kimjunyeob <tislwlstnf1@naver.com>
2023-04-09 22:25:36 +0900

    2024-04-09 bottomSheet 높이조절처리 push

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
+import "package:calendar_scheduler/component/schadule_bottom_sheet.dart";
 import "package:calendar_scheduler/component/schedule_card.dart";
 import "package:calendar_scheduler/component/today_banner.dart";
+import "package:calendar_scheduler/const/colors.dart";
 import "package:flutter/material.dart";
 import "package:calendar_scheduler/component/calendar.dart";
 
@@ -17,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFlaotingActionButton(),
       body: SafeArea(
         child: Column(
           children: [
@@ -34,14 +37,25 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(
               height: 8,
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: ScheduleCard(
-                  startTime: 3, endTime: 4, content: "내용", color: Colors.red),
-            )
+            _ScheduleList(),
           ],
         ),
       ),
+    );
+  }
+
+  FloatingActionButton renderFlaotingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+            context: context,
+            builder: (_) {
+              return ScheduleBottomSheet();
+            });
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: const Icon(Icons.add),
     );
   }
 
@@ -50,5 +64,30 @@ class _HomeScreenState extends State<HomeScreen> {
       this.selectedDay = selectedDay;
       this.focusedDay = selectedDay;
     });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: ListView.separated(
+          itemCount: 3,
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              height: 8,
+            );
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return ScheduleCard(
+                startTime: 3, endTime: 4, content: "내용", color: Colors.red);
+          },
+        ),
+      ),
+    );
   }
 }
