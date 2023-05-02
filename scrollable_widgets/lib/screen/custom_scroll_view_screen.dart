8ea0490ea8fb4bbf52110d5991cbb4182c557544
kimjunyeob <tislwlstnf1@naver.com>
2023-05-02 13:22:09 +0900

    2023-05-02 customScroll Appbar 정리

--- scrollable_widgets/lib/screen/custom_scroll_view_screen.dart
+++ scrollable_widgets/lib/screen/custom_scroll_view_screen.dart
@@ -8,23 +8,41 @@ class CustomScrollViewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text('CustomScrollViewScreen'),
-          ),
-          SliverGrid(
-              delegate:
-                  SliverChildBuilderDelegate(childCount: 100, (context, index) {
-                return renderContainer(
-                    color: rainbowColors[index % rainbowColors.length],
-                    index: index);
-              }),
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200)),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            renderSliverAppbar(),
+            renderBuilderListView(),
+            renderBuilderGridListView(),
+          ],
+        ),
       ),
+    );
+  }
+
+  // AppBar
+  SliverAppBar renderSliverAppbar() {
+    return SliverAppBar(
+      floating: true,
+      // 위로 스크롤 시 나타남
+      pinned: false,
+      // 스크롤해도 계속 고정되게함
+      snap: true,
+      // floating: true로 두고 snap: true면 앱바가 애니메이션 효과로 나타남
+      stretch: true,
+      // 맨 위에서 한 개이상에서 스크롤 시 앱바가 남는 공간을 차지함 ios에서 확인가능
+      expandedHeight: 200,
+      // 앱바의 최대 높이 값,
+      collapsedHeight: 100,
+      // 스크롤 위로 시 앱바의 최소 높이 값,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.asset('asset/img/image_1.jpeg',
+        fit: BoxFit.cover,),
+        title: Text('FlexibleSpaceBar'),
+        centerTitle: true,
+      ),
+      title: Text('CustomScrollViewScreen'),
     );
   }
 
@@ -42,7 +60,7 @@ class CustomScrollViewScreen extends StatelessWidget {
   // 2. ListView 화면에 보이는 영역만 그림
   SliverList renderBuilderListView() {
     return SliverList(
-        delegate: SliverChildBuilderDelegate(childCount: 100, (context, index) {
+        delegate: SliverChildBuilderDelegate(childCount: 20, (context, index) {
       return renderContainer(
           color: rainbowColors[index % rainbowColors.length], index: index);
     }));
