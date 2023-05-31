30bb81686c9c09efd752cb914e0b54ce4efea22e
kimjunyeob <tislwlstnf1@naver.com>
2023-05-31 23:40:46 +0900

    2023-05-31 종류별 통계 front 작업

---
+++ dusty_dust/lib/component/main_stat.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MainStat extends StatelessWidget {
+  final String category;
+  final String imgPath;
+  final String level;
+  final String stat;
+
+  MainStat({required this.category,
+    required this.imgPath,
+    required this.level,
+    required this.stat,
+    Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ts = TextStyle(color: Colors.black);
+
+    return Column(
+      children: [
+        Text(
+          category,
+          style: ts,
+        ),
+        const SizedBox(height: 8.0,),
+        Image.asset(
+          imgPath,
+          width: 50.0,
+        ),
+        const SizedBox(height: 8.0,),
+        Text(level, style: ts,),
+        Text(stat, style: ts,),
+      ],
+    );
+  }
+}
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,63 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: primaryColor,
       drawer: MainDrawer(),
       body: CustomScrollView(
-        slivers: [MainAppBar()],
+        slivers: [
+          MainAppBar(),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                  color: lightColor,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                          decoration: const BoxDecoration(
+                              color: darkColor,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(16.0),
+                                  topRight: Radius.circular(16.0))),
+                          child: const Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              "종류별 통계",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          )),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          MainStat(
+                              category: "미세먼지",
+                              imgPath: "asset/img/best.png",
+                              level: "최고",
+                              stat: "0㎍/㎥"),
+                          MainStat(
+                              category: "미세먼지",
+                              imgPath: "asset/img/best.png",
+                              level: "최고",
+                              stat: "0㎍/㎥"),
+                          MainStat(
+                              category: "미세먼지",
+                              imgPath: "asset/img/best.png",
+                              level: "최고",
+                              stat: "0㎍/㎥")
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
