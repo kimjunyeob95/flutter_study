05c75016f4b3677821b637016257c0c77bc8655a
kimjunyeob <tislwlstnf1@naver.com>
2023-05-31 22:44:18 +0900

    2023-05-31 Drawer 작업

--- dusty_dust/lib/component/main_app_bar.dart
+++ dusty_dust/lib/component/main_app_bar.dart
@@ -2,49 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:dusty_dust/const/colors.dart';
 
 class MainAppBar extends StatelessWidget {
-  MainAppBar({Key? key}) : super(key: key);
+  const MainAppBar({Key? key}) : super(key: key);
 
-  final ts = TextStyle(color: Colors.white, fontSize: 30.0);
+  final ts = const TextStyle(color: Colors.white, fontSize: 30.0);
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
       backgroundColor: primaryColor,
-      expandedHeight: 400.0,
+      expandedHeight: 500.0,
       flexibleSpace: FlexibleSpaceBar(
         background: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                "서울",
-                style: ts,
-              ),
-              Text(
-                DateTime.now().toString(),
-                style: ts.copyWith(fontSize: 20.0),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Image.asset(
-                "asset/img/mediocre.png",
-                width: MediaQuery.of(context).size.width / 2,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "보통",
-                style: ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                "나쁘지 않네요",
-                style: ts.copyWith(fontSize: 20.0, fontWeight: FontWeight.w700),
-              )
-            ],
+          child: Container(
+            margin: const EdgeInsets.only(top: kToolbarHeight),
+            child: Column(
+              children: [
+                Text(
+                  "서울",
+                  style: ts,
+                ),
+                Text(
+                  DateTime.now().toString(),
+                  style: ts.copyWith(fontSize: 20.0),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Image.asset(
+                  "asset/img/mediocre.png",
+                  width: MediaQuery.of(context).size.width / 2,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "보통",
+                  style: ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  "나쁘지 않네요",
+                  style: ts.copyWith(fontSize: 20.0, fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
+      drawer: MainDrawer(),
       body: CustomScrollView(
         slivers: [MainAppBar()],
       ),
